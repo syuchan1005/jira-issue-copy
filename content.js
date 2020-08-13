cc695a046fccfd62f6cb40d248e32adfbef14610
syuchan1005 <syuchan.dev@gmail.com>
2020-08-13 22:48:10 +0900

    Add option popup

--- content.js
+++ content.js
@@ -1,18 +1,18 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'getIssueData') {
-        const numberEl = document.querySelector('#issuekey-val');
-        const numberEl2 = document.querySelector('#key-val');
-        const titleEl = document.querySelector('#summary-val');
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        if (request.action === 'getIssueData') {
+            const numberEl = document.querySelector('#issuekey-val');
+            const numberEl2 = document.querySelector('#key-val');
+            const titleEl = document.querySelector('#summary-val');
 
-        if ((numberEl || numberEl2) && titleEl) {
-            const issueNumber = (numberEl || numberEl2).textContent;
-            const issueTitle = titleEl.textContent;
+            if ((numberEl || numberEl2) && titleEl) {
+                const issueNumber = (numberEl || numberEl2).textContent;
+                const issueTitle = titleEl.textContent;
 
-            sendResponse({ num: issueNumber, title: issueTitle });
-        } else {
-            sendResponse(null);
+                sendResponse({num: issueNumber, title: issueTitle});
+                return;
+            }
         }
-    }
 
-    return true;
-});
+        sendResponse();
+    },);
